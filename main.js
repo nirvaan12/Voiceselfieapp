df4b535f3400414088c9816109d5c2bc6ec6883b
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2021-11-30 19:51:57 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -13,5 +13,25 @@ var content=event.results[0][0].transcript;
 document.getElementById("textbox").innerHTML=content;
 console.log(content);
 
+if(content=="take my selfie"){
+    speech1();
+}
 
 }
+
+
+function speech1(){
+synth= window.speechSynthesis;
+speakdata = "Taking your selfie in 5 seconds";
+var utterThis= new SpeechSynthesisUtterance(speakdata);
+synth.speak(utterThis);
+Webcam.attach(camera);
+}
+
+camera= document.getElementById("camera");
+Webcam.set({
+width:360,
+height:250,
+image_format:"jpeg",
+jpeg_quality:90
+});
